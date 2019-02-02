7b6801720dd23d1fc24a09e7775d7341e1df1f1e
Eugin82 <eugink82@mail.ru>
2019-02-02 18:54:57 +0300

    Change_ArrayStorage(second repair)

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,56 +3,61 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
-    private int numElems=0;
+    private int numElems = 0;
+
     void clear() {
-      for(int i=0;i<numElems;i++)
-          storage[i]=null;
-      numElems=0;
+        for (int i = 0; i < numElems; i++) {
+            storage[i] = null;
+        }
+        numElems = 0;
     }
 
     void save(Resume r) {
-      int i;
-      if(numElems==0) {
-          storage[numElems++] = r;
-      }
-      else {
-          for (i = 0; i < numElems; i++) {
-             if(storage[i].toString().equals(r.toString()))
-               break;
-          }
-          if(i==numElems) {
-              storage[numElems++] = r;
-          }
-      }
+        int i;
+        if (numElems == 0) {
+            storage[numElems++] = r;
+        } else {
+            for (i = 0; i < numElems; i++) {
+                if (storage[i].toString().equals(r.toString())) {
+                    break;
+                }
+            }
+            if (i == numElems) {
+                storage[numElems++] = r;
+            }
+        }
     }
 
     Resume get(String uuid) {
-      Resume getElem=null;
-      for(int i=0;i<numElems;i++)
-          if((storage[i].toString()).equals(uuid))
-            getElem=storage[i];
-      return getElem;
+        for (int i = 0; i < numElems; i++) {
+            if ((storage[i].toString()).equals(uuid)) {
+                return storage[i];
+            }
+        }
+        return null;
     }
 
     void delete(String uuid) {
-      for(int i=0;i<numElems; i++){
-          if((storage[i].toString()).equals(uuid)){
-             for(int j=i;j<numElems;j++){
-                 storage[j]=storage[j+1];
-             }
-             numElems--;
-          }
-      }
+        for (int i = 0; i < numElems; i++) {
+            if ((storage[i].toString()).equals(uuid)) {
+                for (int j = i; j < numElems; j++) {
+                    storage[j] = storage[j + 1];
+                }
+                numElems--;
+                return;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-       Resume[] array=new Resume[numElems];
-        for(int i=0;i<numElems;i++)
-            array[i]=storage[i];
-        return array;
+        Resume[] resumes = new Resume[numElems];
+        for (int i = 0; i < numElems; i++) {
+            resumes[i] = storage[i];
+        }
+        return resumes;
     }
 
     int size() {
