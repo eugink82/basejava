4d8b39b19c17b43a42f329668303445af878c7bd
Eugin82 <eugink82@mail.ru>
2019-02-07 16:12:22 +0300

    HW2_toCheck

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -15,46 +15,68 @@ public class ArrayStorage {
         }
         numElems = 0;
     }
-    public void update(Resume r){
 
+    public void update(Resume r, String newUuid) {
+        String uuid = r.toString();
+        int findNumElem = doExistsResume(uuid);
+        if (findNumElem >= 0) {
+            r.setUuid(newUuid);
+            storage[findNumElem] = r;
+        } else
+            System.out.println("Резюме " + uuid + " невозможно обновить, оно отсутствует в хранилище данных.");
     }
 
     public void save(Resume r) {
         int i;
+        String uuid = r.toString();
+        int findNumElem = -1;
         if (numElems == 0) {
             storage[numElems++] = r;
         } else {
-            for (i = 0; i < numElems; i++) {
-                if (storage[i].toString().equals(r.toString())) {
-                    break;
-                }
-            }
-            if (i == numElems) {
+//            for (i = 0; i < numElems; i++) {
+//                if (storage[i].toString().equals(uuid)) {
+//                    break;
+//                }
+//            }
+            findNumElem = doExistsResume(uuid);
+//            if (i == numElems && numElems < 10000) {
+//                storage[numElems++] = r;
+//            }
+            if (findNumElem == -1 && numElems < 10000) {
                 storage[numElems++] = r;
-            }
+            } else
+                System.out.println("Резюме " + uuid + " уже есть в хранилище в хранилище данных.");
         }
     }
 
     public Resume get(String uuid) {
-        for (int i = 0; i < numElems; i++) {
-            if ((storage[i].toString()).equals(uuid)) {
-                return storage[i];
-            }
+//        for (int i = 0; i < numElems; i++) {
+//            if ((storage[i].toString()).equals(uuid)) {
+//                return storage[i];
+//            }
+//        }
+        int findNumElem = -1;
+        if ((findNumElem = doExistsResume(uuid)) >= 0) {
+            return storage[findNumElem];
+        } else {
+            System.out.println("Резюме " + uuid + " не возможно получить, его нет в хранилище в хранилище данных.");
         }
         return null;
     }
 
     public void delete(String uuid) {
-        for (int i = 0; i < numElems; i++) {
-            if ((storage[i].toString()).equals(uuid)) {
-                for (int j = i; j < numElems; j++) {
-                    storage[j] = storage[j + 1];
-                }
-                numElems--;
-                return;
-            }
-        }
-        System.out.println("Резюме "+uuid+" отсутствует в хранилище данных.");
+//        for (int i = 0; i < numElems; i++) {
+//            if ((storage[i].toString()).equals(uuid)) {
+//                for (int j = i; j < numElems; j++) {
+//                    storage[j] = storage[j + 1];
+//                }
+        int findNumElem = doExistsResume(uuid);
+        if (findNumElem >= 0) {
+            storage[findNumElem] = storage[numElems - 1];
+            storage[numElems - 1] = null;
+            numElems--;
+        } else
+            System.out.println("Резюме " + uuid + " не удалось удалить, так как оно отсутствует в хранилище данных.");
     }
 
     /**
@@ -70,5 +92,15 @@ public class ArrayStorage {
 
     public int size() {
         return numElems;
+    }
+
+    private int doExistsResume(String uuid) {
+
+        for (int i = 0; i < numElems; i++) {
+            if ((storage[i].toString()).equals(uuid)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
