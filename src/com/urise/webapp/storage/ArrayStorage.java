72f12e8c82bd3a00980c05d7d16c4b0eb41c1a89
Eugin82 <eugink82@mail.ru>
2019-02-11 00:34:11 +0300

    Introduce interface

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -5,7 +5,7 @@ import java.util.*;
 /**
  * Array based storage for Resumes
  */
-public class ArrayStorage {
+public class ArrayStorage implements Storage{
     private static final int STORAGE_LIMIT = 10_000;
     private Resume[] storage = new Resume[STORAGE_LIMIT];
     private int numElems = 0;
